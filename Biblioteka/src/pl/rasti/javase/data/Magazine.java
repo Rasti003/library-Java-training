fb06ec7c437319a0ceb27a5c98548ca1e6efffa7
Rasti003 <przemyslaw.murzyn@outlook.com>
2018-02-04 17:29:34 +0100

    Added Date Library

--- Biblioteka/src/pl/rasti/javase/data/Magazine.java
+++ Biblioteka/src/pl/rasti/javase/data/Magazine.java
@@ -1,26 +1,20 @@
 package pl.rasti.javase.data;
 
+import java.time.LocalDate;
+
 public class Magazine extends Publication {
     private static final long serialVersionUID = 2061400934707882805L;
-
-    private int month;
-    private int day;
     private String language;
 
     public int getMonth() {
-        return month;
-    }
-
-    public void setMonth(int month) {
-        this.month = month;
+        return getDate().getMonthValue();
     }
 
     public int getDay() {
-        return day;
+        return getDate().getDayOfMonth();
     }
-
-    public void setDay(int day) {
-        this.day = day;
+    public int getYear() {
+        return getDate().getYear();
     }
 
     public String getLanguage() {
@@ -34,34 +28,20 @@ public class Magazine extends Publication {
     public Magazine(String title, String publisher, String language, int year, int month, int day) {
         super(year, title, publisher);
         setLanguage(language);
-        setMonth(month);
-        setDay(day);
+        setDate(LocalDate.of(year, month, day));
     }
 
     @Override
     public String toString() {
-        StringBuilder print = new StringBuilder(32);
-        print.append(getTitle());
-        print.append("; ");
-        print.append(getPublisher());
-        print.append("; ");
-        print.append(getYear());
-        print.append("; ");
-        print.append(getMonth());
-        print.append("; ");
-        print.append(getDay());
-        print.append("; ");
-        print.append(getLanguage());
-        return print.toString();
+        return getTitle() +  "; " + getPublisher() + "; " + getYear() + "-"
+                + getMonth() + "-" + getDay() + "; " + getLanguage();
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = super.hashCode();
-        result = prime * result + day;
         result = prime * result + ((language == null) ? 0 : language.hashCode());
-        result = prime * result + month;
         return result;
     }
 
@@ -74,14 +54,10 @@ public class Magazine extends Publication {
         if (getClass() != obj.getClass())
             return false;
         Magazine other = (Magazine) obj;
-        if (day != other.day)
-            return false;
         if (language == null) {
             if (other.language != null)
                 return false;
         } else if (!language.equals(other.language))
-            return false;
-        if (month != other.month)
             return false;
         return true;
     }
