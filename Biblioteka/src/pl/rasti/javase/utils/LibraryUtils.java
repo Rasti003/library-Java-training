6e395e85f8026ed3a8922722444ac3b64ec06fb5
Rasti003 <przemyslaw.murzyn@outlook.com>
2018-02-04 16:28:31 +0100

    modified print method to stream

--- Biblioteka/src/pl/rasti/javase/utils/LibraryUtils.java
+++ Biblioteka/src/pl/rasti/javase/utils/LibraryUtils.java
@@ -1,61 +1,32 @@
 package pl.rasti.javase.utils;
 
-;import pl.rasti.javase.data.*;
+import pl.rasti.javase.data.*;
 
-import java.util.*;
+
 
 public class LibraryUtils {
 
-
     public static void printBooks(Library lib) {
-        List<Publication> publications = new ArrayList<>();
-        publications.addAll(lib.getPublications().values());
-        Collections.sort(publications, new Library.AlphabeticalComparator());
-        int countBooks = 0;
-        for (Publication p : publications) {
-            if (p instanceof Book) {
-                System.out.println(p);
-                countBooks++;
-            }
-        }
-
-        if (countBooks == 0) {
-            System.out.println("Brak książek w bibliotece");
-        }
+        printPublications(lib, Book.class);
     }
 
-
     public static void printMagazines(Library lib) {
-        List<Publication> publications = new ArrayList<>();
-        publications.addAll(lib.getPublications().values());
+        printPublications(lib, Magazine.class);
+    }
 
-        Collections.sort(publications, new Library.AlphabeticalComparator());
-        int countMagazines = 0;
-        for (Publication p : publications) {
-            if (p instanceof Magazine) {
-                System.out.println(p);
-                countMagazines++;
-            }
-        }
+    private static void printPublications(Library lib, Class cl) {
+        long countMagazines = lib.getPublications().values().stream()
+                .filter(cl::isInstance).sorted(new Library.AlphabeticalComparator())
+                .peek(System.out::println).count();
 
         if (countMagazines == 0) {
-            System.out.println("Brak magazynów w bibliotece");
+            System.out.println("W bibliotece nie znaleziono publikacji typu " + cl.getSimpleName());
         }
     }
 
     public static void printUsers(Library lib) {
-
-        List<LibraryUser> users = new ArrayList<>();
-        users.addAll(lib.getUsers().values());
-
-        Collections.sort(users, new Comparator<LibraryUser>() {
-            @Override
-            public int compare(LibraryUser o1, LibraryUser o2) {
-                return o1.getLastName().compareTo(o2.getLastName());
-            }
-        });
-        for (LibraryUser u : users) {
-            System.out.println(u);
-        }
+        lib.getUsers().values().stream()
+                .sorted((a, b) -> a.getLastName().compareTo(b.getLastName()))
+                .forEach(System.out::println);
     }
 }
